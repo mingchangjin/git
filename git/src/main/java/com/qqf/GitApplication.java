cc58f7fafe46540169fd171987dbf2147c72adea
mcjptwop <773951914@qq.com>
2017-11-08 10:18:36 +0800

    git

---
+++ git/src/main/java/com/qqf/GitApplication.java
@@ -0,0 +1,12 @@
+package com.qqf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitApplication.class, args);
+	}
+}
---
+++ git/src/main/java/com/qqf/GitContorller.java
@@ -0,0 +1,8 @@
+package com.qqf;
+
+/**
+ * Created by Administrator on 2017/11/8 0008.
+ */
+public class GitContorller {
+    public  void git(){};
+}
